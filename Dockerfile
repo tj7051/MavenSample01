c1dfceba4fe1abfd4e1e042236a9a5922c62e43e
bumkee <bert9403202@naver.com>
2023-08-14 14:45:15 +0900

    first work

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+From openjdk:11-jdk
+
+WORKDIR /app
+
+COPY target/*.jar app.jar
+
+CMD ["java", "-Dspring.profiles.active=prod", "-jar", "app.jar"]
---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	
+	@GetMapping({"/",""})
+	public ResponseEntity<?> index(){
+		Map<String, Object> map = new HashMap<>();
+		map.put("id", "k001");
+		
+		return ResponseEntity.ok().body(map);
+	}
+
+}
